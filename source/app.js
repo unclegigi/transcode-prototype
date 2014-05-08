040d7f8e105ff349040fd5b6f3eaa5c7bc35ce56
Uncle Gigi <unclegigi@outlook.com>
2014-05-08 16:10:36 +0200

    add init app.js code

---
+++ source/app.js
@@ -0,0 +1,175 @@
+
+
+var convert = function(file, callback) {
+
+	console.log(new Date())
+	console.log();
+	var	child_process = require("child_process");
+	var description = { file : file, hd : false,
+		audio : { de : {surround: 0, surround_hd: 0, stereo : 0}, en : { surround: 0, surround_hd: 0, stereo : 0}},
+		subtitle : {de : [], en : [] } };
+	var count = 0;
+	var type = "";
+	var execution = "HandbrakeCLIx64";
+	var inform = execution + " -i \"" + description.file + "\" --scan";
+	var process = execution + " -i \"" + description.file + "\" -o \"" + description.file + ".converted.mkv" + "\" -f mkv";
+
+	child_process.exec(inform, function (informError, informStdout, informStderr) {
+
+		var informOutput = informStderr;
+
+		informOutput.split("\n").forEach(function(line) {
+
+			line = line.toLowerCase();
+
+			if (line.indexOf("+ size: 1920x1080") !== -1) {
+				description.hd = true;
+				return;
+			};
+
+			if (line.indexOf("audio tracks") !== -1) {
+				type = "audio";
+				count = 0;
+				return;
+			};
+
+			if (line.indexOf("subtitle tracks") !== -1) {
+				type = "subtitle";
+				count = 0;
+				return;
+			};
+
+			if (type !== "") {
+				if (line.match(/    \+ (.*)/)) {
+					count++;
+					if (type === "audio") {
+						if (line.indexOf("iso639-2: deu") !== -1) {
+							if ((line.indexOf("5.1") !== -1) || (line.indexOf("7.1") !== -1)) {
+								if (line.indexOf("dts-hd") !== -1) {
+									if (description.audio.de.surround_hd === 0) {
+										description.audio.de.surround_hd = count;
+									}
+								} else {
+									if (description.audio.de.surround === 0) {
+										description.audio.de.surround = count;
+									}
+								};
+							} else {
+								if (description.audio.de.stereo === 0) {
+									description.audio.de.stereo = count;
+								}
+							};
+						} else if (line.indexOf("iso639-2: eng") !== -1) {
+							if ((line.indexOf("5.1") !== -1) || (line.indexOf("7.1") !== -1)) {
+								if (line.indexOf("dts-hd") !== -1) {
+									if (description.audio.en.surround_hd === 0) {
+										description.audio.en.surround_hd = count;
+									}
+								} else {
+									if (description.audio.en.surround === 0) {
+										description.audio.en.surround = count;
+									}
+								};
+							} else {
+								if (description.audio.en.stereo === 0) {
+									description.audio.en.stereo = count;
+								}
+							};
+						};
+					} else if (type === "subtitle") {
+						if (line.indexOf("iso639-2: deu") !== -1) {
+							description.subtitle.de.push(count);
+						} else if (line.indexOf("iso639-2: eng") !== -1) {
+							description.subtitle.en.push(count);
+						};
+					};
+				};
+			};
+
+		});
+
+		console.log(description)
+		console.log();
+		var bitrate = 8000;
+		if (description.hd === false) {
+			bitrate = 4000;
+		}
+
+		var audio_de = "";
+		if (description.audio.de.stereo !== 0) {
+			audio_de = description.audio.de.stereo;
+		};
+
+		if (description.audio.de.surround !== 0) {
+			audio_de = description.audio.de.surround;
+		};
+		
+		if (description.audio.de.surround_hd !== 0) {
+			audio_de = description.audio.de.surround_hd
+		};
+
+		var audio_en = "";
+		if (description.audio.en.stereo !== 0) {
+			audio_en = description.audio.en.stereo;
+		};
+
+		if (description.audio.en.surround !== 0) {
+			audio_en = description.audio.en.surround;
+		};
+		
+		if (description.audio.en.surround_hd !== 0) {
+			audio_en = description.audio.en.surround_hd
+		};
+
+		var audio = "";
+		if (audio_de !== "") {
+			audio = audio_de;
+		}
+		if (audio_en !== "") {
+			if (audio === "") {
+				audio = audio_en;
+			} else {
+				audio += "," + audio_en;
+			}
+		}
+
+		var subtitle = "";
+		description.subtitle.de.forEach(function(i) {
+			if (subtitle === "") {
+				subtitle = i;
+			} else {
+				subtitle = subtitle + "," + i;
+			} 
+		});
+		description.subtitle.en.forEach(function(i) {
+			if (subtitle === "") {
+				subtitle = i;
+			} else {
+				subtitle = subtitle + "," + i;
+			} 
+		});
+
+		process += " --loose-anamorphic --modulus 2 -e x264 -b " + bitrate + " -2 --vfr -a " + audio;
+		process += " -E copy -6 auto -R Auto --subtitle " + subtitle + " --x264-preset=veryfast --verbose=1";
+
+		if (description.hd === true) {
+			process += " --x264-profile=main --h264-level=\"4.1\"";
+		} else {
+			process += " --x264-profile=baseline --h264-level=\"3.0\"";
+		}
+		console.log(process);
+
+		child_process.exec(process, function (processError, processStdout, processStderr) {
+			callback();
+		});
+
+	});
+}
+
+convert("C:\\Filme (Original Eingang)\\Video1.mkv", function() {
+	console.log("Fertig Liberace!");
+	console.log();
+	console.log(new Date())
+	console.log();
+	return;
+});
